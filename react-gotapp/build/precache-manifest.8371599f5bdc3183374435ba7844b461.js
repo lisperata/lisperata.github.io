e973e11330b1c9fc067fc505a2e4c06d326d0f1f
lisperata <lisperata@users.noreply.github.com>
2021-02-12 01:14:25 +0000

    deploy: lisperata/react-gotapp@2c414b461bcd7c8635ba5b442ad6e7582aeec2b8

---
+++ react-gotapp/build/precache-manifest.8371599f5bdc3183374435ba7844b461.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce5943a2ba0cb72b121d",
+    "url": "/static/css/main.e5d4e042.chunk.css"
+  },
+  {
+    "revision": "ce5943a2ba0cb72b121d",
+    "url": "/static/js/main.ce5943a2.chunk.js"
+  },
+  {
+    "revision": "bfd68e0a78941086eb95",
+    "url": "/static/css/1.9d3e7171.chunk.css"
+  },
+  {
+    "revision": "bfd68e0a78941086eb95",
+    "url": "/static/js/1.bfd68e0a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3c5dc859f6522f6e30a14adb936d06c1",
+    "url": "/static/media/error.3c5dc859.jpg"
+  },
+  {
+    "revision": "f376cf5dd996f2702e291d5dfae74431",
+    "url": "/index.html"
+  }
+];
