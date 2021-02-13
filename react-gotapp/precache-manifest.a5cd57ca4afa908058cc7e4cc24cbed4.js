d46370b2f0bb5717135e4588bfcdf3304d74094c
lisperata <lisperata@users.noreply.github.com>
2021-02-13 16:13:50 +0000

    deploy: lisperata/react-gotapp@dd31242801b1a1ae78e54c994b1638ad7ed8ea4d

---
+++ react-gotapp/precache-manifest.a5cd57ca4afa908058cc7e4cc24cbed4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce5943a2ba0cb72b121d",
+    "url": "post-app/static/css/main.e5d4e042.chunk.css"
+  },
+  {
+    "revision": "ce5943a2ba0cb72b121d",
+    "url": "post-app/static/js/main.ce5943a2.chunk.js"
+  },
+  {
+    "revision": "bfd68e0a78941086eb95",
+    "url": "post-app/static/css/1.9d3e7171.chunk.css"
+  },
+  {
+    "revision": "bfd68e0a78941086eb95",
+    "url": "post-app/static/js/1.bfd68e0a.chunk.js"
+  },
+  {
+    "revision": "cb15bdd19c3852baa0c5",
+    "url": "post-app/static/js/runtime~main.cb15bdd1.js"
+  },
+  {
+    "revision": "3c5dc859f6522f6e30a14adb936d06c1",
+    "url": "post-app/static/media/error.3c5dc859.jpg"
+  },
+  {
+    "revision": "b1ae228e63a46c5b56f5da45f33687eb",
+    "url": "post-app/index.html"
+  }
+];
