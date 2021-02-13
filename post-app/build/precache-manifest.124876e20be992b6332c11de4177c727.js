fe07c722028aaad54b9afe3119c7eea593cf4085
lisperata <lisperata@users.noreply.github.com>
2021-02-13 15:12:57 +0000

    deploy: lisperata/post-app@26bab34806b7d981d0a9ccb8f354ec47196ae592

---
+++ post-app/build/precache-manifest.124876e20be992b6332c11de4177c727.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "837442cf091edd527258afebe84d6779",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bfb0c135bfd83527e3c",
+    "url": "/static/css/main.7689019f.chunk.css"
+  },
+  {
+    "revision": "a8c70ac7d82761c7d2dd",
+    "url": "/static/js/2.7595b8a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7595b8a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bfb0c135bfd83527e3c",
+    "url": "/static/js/main.cfc2e31e.chunk.js"
+  },
+  {
+    "revision": "6dc60c1ef4d35e3c22d6",
+    "url": "/static/js/runtime-main.3d1a7b10.js"
+  }
+]);
